f2a8e27a239aed74cf18f70f47b79e55469d6c74
Yeric7 <y3ric7@gmail.com>
2022-05-10 12:03:50 +0200

    Problème de css avec le webpack + impossible d'afficher un mask svg

--- projet/src/index.js
+++ projet/src/index.js
@@ -1,58 +1,8 @@
-/*https://vegibit.c om/create-a-bar-chart-with-d3-javascript/
- */
-//https://www.analyticsvidhya.com/blog/2017/08/visualizations-with-d3-js/
-// Pour importer les données
-// import file from '../villeData/villeData.csv'
-
-//https://www.tutorialsteacher.com/d3js/create-bar-chart-using-d3js
-
-
-
-
-/* // import villedata from '../villeData/CityHeight.csv'
-import eauData from '../villeData/WaterLevel.csv'
-console.log(eauData)
-
-import villeData from '../villeData/CityHeight.csv'
-console.log(villeData)
-
-
-
-console.log(villeData['0'])
-console.log(villeData['Ville'])
-
-//put villedatainto an array
-let villeTab = [] 
-for (let i = 0 i < villeData.length i++) {
-    villeTab.push(villeData[i])
-}
-console.log('oui')
-console.log(villeTab) */
-
-//d
-
-
-/* import villeData from '../data/CityHeight.csv'
-
-
-for (let i = 0 i < villeData.length i++) {
-    console.log((villeData[i]))
-}
-
-console.log('oui')
-console.log(villeData.Hauteur)
-console.log(villeData.map(d => d.Hauteur))
-
-
-import eauData from '../data/WaterLevel.csv'
-console.log(eauData.map(b => b.hauteurCumnul))
-console.log(eauData)
- */
-
-
 import * as d3 from 'd3'
+
+import "style.css"
 /* import { csv } from 'd3' */
-import { csv, json } from 'd3-fetch'
+/* import { csv, json } from 'd3-fetch' */
 import cityHeightData from "../data/cityHeight.csv"
 import waterLevelData from "../data/WaterLevel.csv"
 import citationTextData from "../data/CitationText.csv"
--- projet/webpack.config.js
+++ projet/webpack.config.js
@@ -1,29 +1,50 @@
-const path = require('path');const HtmlWebpackPlugin = require('html-webpack-plugin');
+const svgToMiniDataURI = require("mini-svg-data-uri");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-    template: './public/index.html',
-    filename: 'index.html',
-    inject: 'body'
-})
+  template: "./public/index.html",
+  filename: "index.html",
+  inject: "body",
+});
 
 module.exports = {
-    name: 'browser',
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve('dist'),
-        filename: 'index_bundle.js'
-    },
-    module: {
-        rules: [
-            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-            {test: /\.csv$/, loader: 'csv-loader', options: {
-                    dynamicTyping: true,
-                    header: true,
-                    skipEmptyLines: true
-                }
-            }
-        ]
-    },
-    plugins: [HtmlWebpackPluginConfig]
-}
+  name: "browser",
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, loader: "babel-loader", exclude: /node_modules/ },
+      {
+        test: /\.csv$/,
+        loader: "csv-loader",
+        options: {
+          dynamicTyping: true,
+          header: true,
+          skipEmptyLines: true,
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.svg$/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              generator: (content) => svgToMiniDataURI(content.toString()),
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [HtmlWebpackPluginConfig],
+};
